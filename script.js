7c561c726b93f30fb4dbf4ea364e0472c06928ad
craftlinker <starterbee2@gmail.com>
2019-02-20 17:48:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function idCard() {
+    var firstName = document.getElementById('firstName').value;
+    var LastName = document.getElementById('lastName').value;
+    
+    var age = document.getElementById('age').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    var zip = document.getElementById('zip').value;
+
+
+    var postFullName = document.getElementById("postFullName");
+    
+    var postAge = document.getElementById("postAge");
+    var postCity = document.getElementById("postCity");
+    var postState = document.getElementById("postState");
+    var postZip = document.getElementById("postZip");
+
+
+
+    postFullName.innerText = firstName+ " " + "" + LastName;
+    
+    postCity.innerText = city;
+    postState.innerText = state;
+    postZip.innerText = zip;
+   
+var ages = [100,9000];
+ages.push(age);
+
+
+for (var i = 0; i < ages.length; i++) {
+    if(parseInt(ages[i]) <= 100) {
+        postAge.innerText = ages[i]; 
+        }
+      }
+
+    }
+
+    // } else if (numberArray >= 100) {
+    //     (document.getElementById("postphoneNumber").innerHTML = "phoneNumger:" + age); 
+    //     }
+
+
+    
